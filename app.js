1ae67db78a6c2ef99aca0e4380d84327a5d6e02e
CristianKB89 <cristianiksl@gmail.com>
2022-01-27 23:30:37 -0500

    Ejercicio10 completo

--- app.js
+++ app.js
@@ -3,31 +3,33 @@ const app = express();
 const port = 3030;
 const path = require('path');
 
+app.use(express.static('public'));
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+}); 
+app.get('/babbage',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/babbage.html'));
+});
+app.get('/berners-lee',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/berners-lee.html'));
+});
+app.get('/clarke',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/clarke.html'));
+});
+app.get('/hamilton',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/hamilton.html'));
+});
+app.get('/hopper',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/hopper.html'));
+});
+app.get('/lovelace',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/lovelace.html'));
+});
+app.get('/turing',(req,res) => {
+    res.sendFile(path.join(__dirname, './views/turing.html'));
+});
 
 app.listen(port,() => {
     console.log('Servidor corriendo en el puerto '+ port);
 });
-app.use('/',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/index.hmtl'));
-});
-/* app.use('/babbage',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/babbage.hmtl'));
-});
-app.use('/berners-lee',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/berners-lee.hmtl'));
-});
-app.use('/clarke',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/clarke.hmtl'));
-});
-app.use('/hamilton',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/hamilton.hmtl'));
-});
-app.use('/hopper',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/hopper.hmtl'));
-});
-app.use('/lovelace',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/lovelace.hmtl'));
-});
-app.use('/turing',(req,res) => {
-    res.sendFile(path.join(__dirname, './views/turing.hmtl'));
-}); */
